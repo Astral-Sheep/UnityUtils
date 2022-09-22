d64537357d7507f52c62f0bf7cd81332518226e3
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-09-22 11:09:55 +0200

    feat: add method Abs in class VectorT, and add dependency UnityEngine.CoreModule.dll

--- src/VectorT.cs
+++ src/VectorT.cs
@@ -1,4 +1,30 @@
+using UnityEngine;
+
 namespace Com.Surbon.UnityUtils.Math
 {
+	public static class VectorT
+	{
+		/// <summary>
+		/// Returns the given vector with absolute values
+		/// </summary>
+		public static Vector2 Abs(Vector2 vector)
+		{
+			return new Vector2(
+				Mathf.Abs(vector.x),
+				Mathf.Abs(vector.y)
+				);
+		}
 
+		/// <summary>
+		/// Returns the given vector with absolute values
+		/// </summary>
+		public static Vector3 Abs(Vector3 vector)
+		{
+			return new Vector3(
+				Mathf.Abs(vector.x),
+				Mathf.Abs(vector.y),
+				Mathf.Abs(vector.z)
+				);
+		}
+	}
 }
