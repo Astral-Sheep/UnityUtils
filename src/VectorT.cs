057b35a2749fa1c7225e9b8aa74a77743111d062
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-09-22 11:17:13 +0200

    feat: add methods CartesianToPolar, CartesianToCylindric and CartesianToSpheric in class VectorT

--- src/VectorT.cs
+++ src/VectorT.cs
@@ -103,5 +103,70 @@ namespace Com.Surbon.UnityUtils.Math
 
 			return new Vector3(x / weight, y / weight, z / weight);
 		}
+
+		/// <summary>
+		/// Returns the polar coordinates of the vector given in cartesian coordinates
+		/// </summary>
+		/// <param name="vector">Cartesian coordinates as (x, y)</param>
+		/// <returns>Polar coordinates as (r, th) with th in radians</returns>
+		public static Vector2 CartesianToPolar(Vector2 vector)
+		{
+			return new Vector2(vector.magnitude, Mathf.Atan2(vector.y, vector.x));
+		}
+
+		/// <summary>
+		/// Returns the polar coordinates of the vector given in cartesian coordinates
+		/// </summary>
+		/// <returns>Polar coordinates as (r, th) with th in radians</returns>
+		public static Vector2 CartesianToPolar(float x, float y)
+		{
+			return new Vector2(Mathf.Sqrt(x * x + y * y), Mathf.Atan2(y, x));
+		}
+
+		/// <summary>
+		/// Returns the cylindric coordinates of the vector from the cartesian coordinates
+		/// </summary>
+		/// <param name="vector">Cartesian coordinates as (x, y, z)</param>
+		/// <returns>Cylindric coordinates as (r, y, phi) with phi in radians</returns>
+		public static Vector3 CartesianToCylindric(Vector3 vector)
+		{
+			return new Vector3(Mathf.Sqrt(vector.x * vector.x + vector.z * vector.z), vector.y, Mathf.Atan2(vector.z, vector.y));
+		}
+
+		/// <summary>
+		/// Returns the cylindric coordinates of the vector given in cartesian coordinates
+		/// </summary>
+		/// <returns>Cylindric coordinates as (r, y, phi) with phi in radians</returns>
+		public static Vector3 CartesianToCylindric(float x, float y, float z)
+		{
+			return new Vector3(Mathf.Sqrt(x * x + z * z), y, Mathf.Atan2(z, x));
+		}
+
+		/// <summary>
+		/// Returns the spheric coordinates of the vector from the cartesian coordinates
+		/// </summary>
+		/// <param name="vector">Cartesian coordinates as (x, y, z)</param>
+		/// <returns>Spheric coordinates as (rho, phi, th) with phi and th in radians</returns>
+		public static Vector3 CartesianToSpheric(Vector3 vector)
+		{
+			return new Vector3(
+				vector.magnitude,
+				Mathf.Atan2(vector.z, vector.x),
+				Mathf.Atan2(vector.y, Mathf.Sqrt(vector.x * vector.x + vector.z * vector.z))
+				);
+		}
+
+		/// <summary>
+		/// Returns the spheric coordinates of the vector from the cartesian coordinates
+		/// </summary>
+		/// <returns>Spheric coordinates as (rho, phi, th) with phi and th in radians</returns>
+		public static Vector3 CartesianToSpheric(float x, float y, float z)
+		{
+			return new Vector3(
+				Mathf.Sqrt(x * x + y * y + z * z),
+				Mathf.Atan2(z, x),
+				Mathf.Atan2(y, Mathf.Sqrt(x * x + z * z))
+				);
+		}
 	}
 }
