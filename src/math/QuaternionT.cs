1d5308678ab6ad45bb2bc5599d5ff74c5d1df1a2
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-09-22 18:53:01 +0200

    feat: add method Rotate in class QuaternionT

--- src/math/QuaternionT.cs
+++ src/math/QuaternionT.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 namespace Com.Surbon.UnityUtils.Math
 {
 	/// <summary>
-	/// Provides methods for <see cref="Quaternion"/>
+	/// Provides methods for <see cref="Quaternion"/>.
 	/// </summary>
 	public static class QuaternionT
 	{
-
+		/// <summary>
+		/// Returns the given quaternion rotated by the given angle on the given axis.
+		/// </summary>
+		public static Quaternion Rotate(Quaternion q, float angle, Vector3 axis)
+		{
+			return q * Quaternion.AngleAxis(angle, axis);
+		}
 	}
 }
