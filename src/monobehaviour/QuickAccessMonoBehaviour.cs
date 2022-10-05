d3d563720de634e3de1ecd57a11b5df5624f001f
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-10-05 10:52:38 +0200

    feat: add properties Position, LocalPosition, Rotation, LocalRotation and LocalScale in class QuickAccessMonoBehaviour

--- src/monobehaviour/QuickAccessMonoBehaviour.cs
+++ src/monobehaviour/QuickAccessMonoBehaviour.cs
@@ -4,6 +4,34 @@ namespace Com.Surbon.UnityUtils.src.monobehaviour
 {
 	public class QuickAccessMonoBehaviour : MonoBehaviour
 	{
+		public Vector3 Position
+		{
+			get => transform.position;
+			set => transform.position = value;
+		}
 
+		public Vector3 LocalPosition
+		{
+			get => transform.localPosition;
+			set => transform.localPosition = value;
+		}
+
+		public Quaternion Rotation
+		{
+			get => transform.rotation;
+			set => transform.rotation = value;
+		}
+
+		public Quaternion LocalRotation
+		{
+			get => transform.localRotation;
+			set => transform.localRotation = value;
+		}
+
+		public Vector3 LocalScale
+		{
+			get => transform.localScale;
+			set => transform.localScale = value;
+		}
 	}
 }
