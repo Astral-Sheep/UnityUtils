5adc88887eda1dacccbe2e482c591f760b673de0
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-10-05 12:32:15 +0200

    feat: add class QuickAccess2DMonoBehaviour, and add properties LocalPosition2D, LocalRotation2D, LocalScale2D, Position2D and Rotation2D in class QuickAccess2DMonoBehaviour

---
+++ src/monobehaviour/QuickAccess2DMonoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Com.Surbon.UnityUtils.Mono
+{
+	public class QuickAccess2DMonoBehaviour : MonoBehaviour
+	{
+		public Vector2 LocalPosition2D
+		{
+			get => new Vector2(transform.localPosition.x, transform.localPosition.y);
+			set => transform.localPosition = new Vector3(value.x, value.y, transform.localPosition.z);
+		}
+
+		public float LocalRotation2D
+		{
+			get => transform.localEulerAngles.z;
+			set => transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, value);
+		}
+
+		public Vector2 LocalScale2D
+		{
+			get => new Vector2(transform.localScale.x, transform.localScale.y);
+			set => transform.localScale = new Vector3(value.x, value.y, transform.localScale.z);
+		}
+
+		public Vector2 Position2D
+		{
+			get => new Vector2(transform.position.x, transform.position.y);
+			set => transform.position = new Vector3(value.x, value.y, transform.position.z);
+		}
+
+		public float Rotation2D
+		{
+			get => transform.eulerAngles.z;
+			set => transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, value);
+		}
+	}
+}
