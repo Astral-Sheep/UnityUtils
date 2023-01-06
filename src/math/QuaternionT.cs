053d3a2d2ad264828e764d05346491b57afc0d22
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2023-01-06 14:35:20 +0100

    feat: add methods GetAngle and GetAxis in class QuaternionT

--- src/math/QuaternionT.cs
+++ src/math/QuaternionT.cs
@@ -8,11 +8,28 @@ namespace Com.Surbon.UnityUtils.Math
 	public static class QuaternionT
 	{
 		/// <summary>
-		/// Returns the given quaternion rotated by the given angle on the given axis.
+		/// Returns the given <see cref="Quaternion"/> rotated by the given angle on the given axis.
 		/// </summary>
 		public static Quaternion Rotate(Quaternion q, float angle, Vector3 axis)
 		{
 			return q * Quaternion.AngleAxis(angle, axis);
+		}
+
+		/// <summary>
+		/// Returns the angle of the given <see cref="Quaternion"/>.
+		/// </summary>
+		/// <returns>The angle in radians.</returns>
+		public static float GetAngle(Quaternion q)
+		{
+			return Mathf.Acos(q.w) * 2f;
+		}
+
+		/// <summary>
+		/// Returns the axis of the given <see cref="Quaternion"/>
+		/// </summary>
+		public static Vector3 GetAxis(Quaternion q)
+		{
+			return new Vector3(q.x, q.y, q.z) / Mathf.Sin(Mathf.Acos(q.w));
 		}
 	}
 }
