43479208b91db28ff060d4817cd85dbb0421cf3e
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2023-01-06 17:34:34 +0100

    refactor: remove useless usings

--- src/math/Range.cs
+++ src/math/Range.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-
-namespace Com.Surbon.UnityUtils.src.math
+﻿namespace Com.Surbon.UnityUtils.src.math
 {
 	/// <summary>
 	/// Representation of a range of numbers.
