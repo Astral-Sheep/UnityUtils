7b6fd6b0a8d7c3218c6801363134ce686cd348bb
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2023-01-06 15:38:03 +0100

    feat: add methods BelongsTo and GetRandom in struct Range

--- src/math/Range.cs
+++ src/math/Range.cs
@@ -1,11 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace Com.Surbon.UnityUtils.src.math
 {
-	public class Range
+	/// <summary>
+	/// Representation of a range of numbers.
+	/// </summary>
+	public struct Range
 	{
+		/// <summary>
+		/// The minimum value of the range.
+		/// </summary>
+		public float Min
+		{
+			get => _min;
+			set
+			{
+				if (value > _max)
+					value = _max;
 
+				_min = value;
+			}
+		}
+
+		/// <summary>
+		/// The maximum value of the range.
+		/// </summary>
+		public float Max
+		{
+			get => _max;
+			set
+			{
+				if (value < _min)
+					value = _min;
+
+				_max = value;
+			}
+		}
+
+		/// <summary>
+		/// The internal minimum value of the range.
+		/// </summary>
+		private float _min;
+		/// <summary>
+		/// The internal maximum value of the range.
+		/// </summary>
+		private float _max;
+
+		/// <summary>
+		/// Says if the given number belongs to the range.
+		/// </summary>
+		public bool BelongsTo(float n)
+		{
+			return n >= _min && n <= _max;
+		}
+
+		/// <summary>
+		/// Returns a random number between the minimum and the maximum.
+		/// </summary>
+		public float GetRandom()
+		{
+			return UnityEngine.Random.Range(_min, _max);
+		}
+
+		/// <summary>
+		/// Says if the given number belongs to the given range.
+		/// </summary>
+		public static bool BelongsTo(float n, float min, float max)
+		{
+			return n >= min && n <= max;
+		}
 	}
 }
